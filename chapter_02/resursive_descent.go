e40843078ed64db10979ff3e21b359fb2af7e1e5
Ashwin Chatterji <ashwinath@hotmail.com>
2021-12-10 11:53:15 +0000

    Bracket matching

--- chapter_02/resursive_descent.go
+++ chapter_02/resursive_descent.go
@@ -11,6 +11,17 @@ type parser struct {
 	err      error
 }
 
+func (p *parser) match(symbol rune) {
+	if p.token != symbol {
+		p.err = fmt.Errorf("expected %c, got %c", symbol, p.token)
+	}
+
+	// Only advance if there is no error or else we get an index out of range panic
+	if p.err == nil {
+		p.getToken()
+	}
+}
+
 func (p *parser) getToken() {
 	p.token = p.value[p.position]
 	p.position++
@@ -34,5 +45,23 @@ func (p *parser) s1() {
 func (p *parser) PositiveNegativeA() error {
 	p.getToken()
 	p.s1()
+	return p.err
+}
+
+func (p *parser) s2() {
+	if p.token == '(' {
+		p.match('(')
+		p.s2()
+		p.match(')')
+		p.s2()
+	} else if p.token != '\n' && p.token != '(' && p.token != ')' {
+		p.err = fmt.Errorf("unexpected symbol %c", p.token)
+	}
+}
+
+// S -> S ( S ) S | ε
+func (p *parser) Brackets() error {
+	p.getToken()
+	p.s2()
 	return p.err
 }
--- chapter_02/resursive_descent_test.go
+++ chapter_02/resursive_descent_test.go
@@ -56,3 +56,65 @@ func TestPostiveNegativeA(t *testing.T) {
 		})
 	}
 }
+
+func TestMatchingBrackets(t *testing.T) {
+	var tests = []struct {
+		name       string
+		testString string
+		hasError   bool
+	}{
+		{
+			name:       "nominal empty",
+			testString: "\n",
+			hasError:   false,
+		},
+		{
+			name:       "nominal",
+			testString: "()\n",
+			hasError:   false,
+		},
+		{
+			name:       "nominal nested",
+			testString: "(((((()())))))\n",
+			hasError:   false,
+		},
+		{
+			name:       "nominal chained",
+			testString: "()()()()\n",
+			hasError:   false,
+		},
+		{
+			name:       "failure",
+			testString: "c\n",
+			hasError:   true,
+		},
+		{
+			name:       "failure unmatching short",
+			testString: "(\n",
+			hasError:   true,
+		},
+		{
+			name:       "failure nested long",
+			testString: "((((())))\n",
+			hasError:   true,
+		},
+		{
+			name:       "failure consecutive long",
+			testString: "()(()\n",
+			hasError:   true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := parser{
+				value: stringToRuneList(tt.testString),
+			}
+			err := p.Brackets()
+			if !tt.hasError {
+				assert.Nil(t, err)
+			} else {
+				assert.NotNil(t, err)
+			}
+		})
+	}
+}
